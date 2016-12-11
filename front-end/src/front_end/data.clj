be2df5b1eb8203fa0844517962e7eac1e086617d
iamrobs <rhayward20@gmail.com>
2016-12-11 22:14:12 +0000

    Update data.clj

--- front-end/src/front_end/data.clj
+++ front-end/src/front_end/data.clj
@@ -4,13 +4,13 @@
             [org.httpkit.client :as http]))
 
 (def quote-service-url
-  (utils/config "QUOTE_SERVICE_URL" "http://localhost:8080"))
+  (utils/config "QUOTE_SERVICE_URL" "http://10.128.0.3:8080"))
 
 (def newsfeed-service-url
-  (utils/config "NEWSFEED_SERVICE_URL" "http://localhost:8080"))
+  (utils/config "NEWSFEED_SERVICE_URL" "http://10.128.0.2:8080"))
 
 (def newsfeed-token
-  (utils/config "NEWSFEED_SERVICE_TOKEN" ""))
+  (utils/config "NEWSFEED_SERVICE_TOKEN" "T1&eWbYXNWG1w1^YGKDPxAWJ@^et^&kX"))
 
 (def options {:as         :text
               :timeout    1000
